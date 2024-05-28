7c05fbf0a3f278d6fa1d8b2fb9534cbf28c3071d
faustine-gbd <111750650+faustine-gbd@users.noreply.github.com>
2024-05-28 13:52:07 +0200

    connexion websocket après l'ID du serveur recu

--- main.js
+++ main.js
@@ -1,52 +1,54 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main');
-const os = require('os');
-const http = require('http');
-const path = require('path');
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+const os = require("os");
+const http = require("http");
+const path = require("path");
+const WebSocket = require("ws");
 
 let mainWindow;
+let ws;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'), // Préchargement du script pour l'exposition d'objets
+      preload: path.join(__dirname, "preload.js"), // Préchargement du script pour l'exposition d'objets
     },
   });
 
-  ipcMain.on('set-title', (event, title) => {
+  ipcMain.on("set-title", (event, title) => {
     const webContents = event.sender;
     const win = BrowserWindow.fromWebContents(webContents);
     win.setTitle(title);
   });
 
-  ipcMain.on('send-partner-id', (event, partnerId) => {
+  ipcMain.on("send-partner-id", (event, partnerId) => {
     sendConnectionRequest(partnerId);
   });
 
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
   // Récupérer l'adresse IP et le nom de l'ordinateur
   const networkInterfaces = os.networkInterfaces();
   let wiFiInterface;
-  if (os.platform() === 'win32') {
-    wiFiInterface = networkInterfaces['Wi-Fi'];
-  } else if (os.platform() === 'darwin') {
-    wiFiInterface = networkInterfaces['en0'] || networkInterfaces['en1'];
+  if (os.platform() === "win32") {
+    wiFiInterface = networkInterfaces["Wi-Fi"];
+  } else if (os.platform() === "darwin") {
+    wiFiInterface = networkInterfaces["en0"] || networkInterfaces["en1"];
   }
 
-  let ipAddress = '';
+  let ipAddress = "";
   if (wiFiInterface) {
     const matchingInterface = wiFiInterface.find(
-        (iface) => iface.family === 'IPv4' && iface.address !== '127.0.0.1'
+      (iface) => iface.family === "IPv4" && iface.address !== "127.0.0.1"
     );
     if (matchingInterface) {
       ipAddress = matchingInterface.address;
@@ -57,7 +59,7 @@ app.whenReady().then(() => {
     console.error("L'interface Wi-Fi n'est pas disponible.");
   }
 
-  console.log('IP Address: ', ipAddress);
+  console.log("IP Address: ", ipAddress);
   const nomPC = os.hostname();
   console.log("Nom de l'ordinateur:", nomPC);
   console.log("Adresse IP:", ipAddress);
@@ -66,37 +68,56 @@ app.whenReady().then(() => {
   sendIdentificationRequest(nomPC, ipAddress);
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 function sendIdentificationRequest(nomPC, ipAddress) {
   const options = {
-    hostname: '192.168.1.27',
+    hostname: "192.168.1.27",
     port: 8000,
-    path: '/demande-identifiants',
-    method: 'POST',
+    path: "/demande-identifiants",
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   };
 
   const req = http.request(options, (res) => {
     if (res.statusCode === 200) {
-      res.on('data', (chunk) => {
+      res.on("data", (chunk) => {
         const responseData = JSON.parse(chunk);
         const ID = responseData.ID;
-        console.log('ID reçu du serveur:', ID);
-        mainWindow.webContents.send('set-id', ID);
+        console.log("ID reçu du serveur:", ID);
+        mainWindow.webContents.send("set-id", ID);
+        // Établir la connexion WebSocket avec le serveur
+        ws = new WebSocket(`ws://192.168.1.27:8081`);
+
+        ws.on("open", () => {
+          console.log("Connexion WebSocket établie");
+          ws.send(JSON.stringify({ type: "register", ID }));
+        });
+
+        ws.on("message", (message) => {
+          console.log("Message reçu du serveur:", message);
+        });
+
+        ws.on("close", () => {
+          console.log("Connexion WebSocket fermée");
+        });
+
+        ws.on("error", (error) => {
+          console.error("Erreur de connexion WebSocket:", error);
+        });
       });
     } else {
-      console.error('Erreur de connexion:', res.statusCode);
+      console.error("Erreur de connexion:", res.statusCode);
     }
   });
 
-  req.on('error', (err) => {
+  req.on("error", (err) => {
     console.error("Erreur lors de l'envoi de la demande:", err);
   });
 
@@ -106,28 +127,28 @@ function sendIdentificationRequest(nomPC, ipAddress) {
 
 function sendConnectionRequest(partnerId) {
   const options = {
-    hostname: '192.168.1.27',
+    hostname: "192.168.1.27",
     port: 8000,
-    path: '/connexion',
-    method: 'POST',
+    path: "/connexion",
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   };
 
   const req = http.request(options, (res) => {
     if (res.statusCode === 200) {
-      res.on('data', (chunk) => {
+      res.on("data", (chunk) => {
         const responseData = JSON.parse(chunk);
-        console.log('Réponse du serveur:', responseData.message);
+        console.log("Réponse du serveur:", responseData.message);
       });
     } else {
-      console.error('Erreur de connexion:', res.statusCode);
+      console.error("Erreur de connexion:", res.statusCode);
     }
   });
 
-  req.on('error', (err) => {
-    console.error('Erreur lors de l\'envoi de la demande:', err);
+  req.on("error", (err) => {
+    console.error("Erreur lors de l'envoi de la demande:", err);
   });
 
   req.write(JSON.stringify({ ID: partnerId }));
